b4cc8b9826ff7ecd023323720d70686a7fe68eb0
生方貴大 <>
2025-07-14 23:58:35 +0900

    いいねをajax化

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,24 +25,3 @@ require("@rails/activestorage").start()
 
 require("trix")
 require("@rails/actiontext")
-
-import $ from 'jquery'
-import axios from 'axios'
-
-const handleHeartDisplay = (hasLiked) => {
-  if (hasLiked) {
-    $('.active-heart').removeClass('hidden')
-  } else {
-    $('.inactive-heart').removeClass('hidden')
-  }
-}
-
-document.addEventListener('turbo:load', () => {
-  const dataset = $('#article-show').data()
-  const articleId = dataset.articleId
-  axios.get(`/articles/${articleId}/like`)
-    .then((response) => {
-      const hasLiked = response.data.hasLiked
-      handleHeartDisplay(hasLiked)
-    })
-})
--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "jquery" # @3.7.1
 pin "axios", to: "https://cdn.skypack.dev/axios@0.21.4"
+pin "@rails/ujs", to: "@rails--ujs.js" # @7.1.3
