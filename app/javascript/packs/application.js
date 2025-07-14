bcef657649792a380cbedaa297fb329e70878f0c
生方貴大 <>
2025-07-14 23:14:52 +0900

    いいね表示をajax化

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,11 +29,20 @@ require("@rails/actiontext")
 import $ from 'jquery'
 import axios from 'axios'
 
+const handleHeartDisplay = (hasLiked) => {
+  if (hasLiked) {
+    $('.active-heart').removeClass('hidden')
+  } else {
+    $('.inactive-heart').removeClass('hidden')
+  }
+}
+
 document.addEventListener('turbo:load', () => {
-  $('.article_title').on('click', () => {
-    axios.get(('/'))
-      .then((response) => {
-        console.log(response)
-      })
-  })
+  const dataset = $('#article-show').data()
+  const articleId = dataset.articleId
+  axios.get(`/articles/${articleId}/like`)
+    .then((response) => {
+      const hasLiked = response.data.hasLiked
+      handleHeartDisplay(hasLiked)
+    })
 })
