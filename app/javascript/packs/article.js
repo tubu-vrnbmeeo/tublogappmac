ae84cdbd5b4d85139b8a2f92fdcadd783efd1f3b
生方貴大 <>
2025-07-30 21:32:30 +0900

    apiルーティング

--- app/javascript/packs/article.js
+++ app/javascript/packs/article.js
@@ -30,7 +30,7 @@ document.addEventListener('turbo:load', () => {
   const dataset = $('#article-show').data()
   const articleId = dataset.articleId
 
-  axios.get(`/articles/${articleId}/comments`)
+  axios.get(`/api/articles/${articleId}/comments`)
     .then((response) => {
       const comments = response.data
       comments.forEach((comment) => {
@@ -45,7 +45,7 @@ document.addEventListener('turbo:load', () => {
     if(!content){
       window.alert('コメントを入力してください')
     } else {
-      axios.post(`/articles/${articleId}/comments`, {
+      axios.post(`/api/articles/${articleId}/comments`, {
         comment: {content: content}
       })
         .then((response) => {
@@ -56,7 +56,7 @@ document.addEventListener('turbo:load', () => {
     }
   })
 
-  axios.get(`/articles/${articleId}/like`)
+  axios.get(`/api/articles/${articleId}/like`)
     .then((response) => {
       const hasLiked = response.data.hasLiked
       handleHeartDisplay(hasLiked)
